f499ec68377e8fcf1833ebb5de10e5fd4517b12b
Jaime Sanders <jaimelyn517@gmail.com>
2020-09-18 16:57:03 -0500

    added chance

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
+const chance = require('chance').Chance();
+
 exports.handler = () => {
-  console.log('hello i am a lambda with zip upload');
+  console.log(`hello i am a lambda with randomness ${chance.name()}`);
 };
